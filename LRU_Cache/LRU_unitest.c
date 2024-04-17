c3de7b066b055a0c1524ef3e80a512ec77946737
Yichi Chao <katelyn.c0818@gmail.com>
2024-04-17 19:10:36 +0800

    Initialize LRU Cache Mechanism
    The code implements the get and put functions for the LRU.
    Additionally, basic correctness tests are included to verify the
    functionality of LRU operations.

---
+++ LRU_Cache/LRU_unitest.c
@@ -0,0 +1,46 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lru.h"
+/* typedef struct {
+    int capacity;
+    int count;
+    struct list_head dhead;
+    struct hlist_head hhead[];
+} LRUCache; */
+
+/*typedef struct {
+    int key;
+    int value;
+    struct hlist_node node;
+    struct list_head link;
+} LRUNode*/
+
+void printCache(LRUCache *obj)
+{
+    struct list_head *cache = &obj->dhead;
+    struct list_head *ptr;
+    printf("==PRINT CACHE==\n");
+    printf("[HEAD]->");
+    list_for_each (ptr, cache) {
+        LRUNode *node = list_entry(ptr, LRUNode, link);
+        int value = node->value;
+        printf("[%d]->", value);
+    }
+    printf("\n===============\n");
+    return;
+}
+int main()
+{
+    LRUCache *Cache = lRUCacheCreate(2);
+    lRUCachePut(Cache, 1, 1);
+    lRUCachePut(Cache, 2, 2);
+    printf("Cache Get: %d\n", lRUCacheGet(Cache, 1));
+    printf("Cache Get: %d\n", lRUCacheGet(Cache, 2));
+    printCache(Cache);
+    lRUCachePut(Cache, 3, 3);
+    printf("Cache Get: %d\n", lRUCacheGet(Cache, 1));
+    printCache(Cache);
+    lRUCacheFree(Cache);
+    return 0;
+}
